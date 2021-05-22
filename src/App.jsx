a9f2879e3cc59071d61581276e086a6b9bef375b
sahmmie <ibrahim.samson@softescu.com>
2021-05-23 00:30:53 +0100

    test covrage frix

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">
-          Learn React docker deploy and step by step builds
+          Learn React docker deploy and step by step builds, test.
         </a>
       </header>
     </div>
