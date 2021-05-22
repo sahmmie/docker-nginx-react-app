d0a21ac984c2ffd8e78460473a2ba34037f7820a
sahmmie <ibrahim.samson@softescu.com>
2021-05-23 00:12:36 +0100

    fixes i dont know part-3

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">
-          Learn React docker deploy and step by step buulds
+          Learn React docker deploy and step by step builds
         </a>
       </header>
     </div>
